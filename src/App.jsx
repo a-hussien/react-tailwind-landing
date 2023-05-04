53d3973df90db3f53a186b60c8fce68dc3669e82
Ahmed Hussein <a.hussien87@gmail.com>
2023-05-04 21:39:02 +0300

    Inintialize the project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-100">
+      <h2 className="py-4 uppercase font-bolder text-4xl text-center">App</h2>
+    </div>
+  )
+}
+
+export default App
